80f741fb18a2bb937f8c9c657c3acdde4d4b14dc
repleeka <tungondugi@gmail.com>
2021-05-26 15:35:49 +0530

    Defined more constants

--- kip_app/lib/shared/formConstants.dart
+++ kip_app/lib/shared/formConstants.dart
@@ -6,7 +6,27 @@ import 'package:flutter/material.dart';
 
 ///[TextInputDecoration Constant]
 ///
+///
+///[cursorColor]
+final cursorColor = Colors.white;
+
 ///Decoration constant for the [FormFields]
-final textInputDecoration = InputDecoration(
-    //TODO:will define more stylings in future.
-    );
+final textInputDecorationForSignUpForm = InputDecoration(
+  hintStyle: TextStyle(
+    color: Colors.grey[400],
+  ),
+  //TODO:will define more stylings in future.
+  focusedBorder: UnderlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.white,
+    ),
+  ),
+);
+final textInputDecorationForSignInForm = InputDecoration(
+  //TODO:will define more stylings in future.
+  focusedBorder: UnderlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.teal,
+    ),
+  ),
+);
