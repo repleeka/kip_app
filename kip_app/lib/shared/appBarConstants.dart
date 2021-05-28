8c6ae8a95a142e6d1f3f976b71c0c888e8152eeb
repleeka <tungondugi@gmail.com>
2021-05-28 21:11:55 +0530

    written more constants

--- kip_app/lib/shared/appBarConstants.dart
+++ kip_app/lib/shared/appBarConstants.dart
@@ -2,4 +2,17 @@ import 'package:flutter/material.dart';
 
 ///[A container for the appBar variable definition.]
 
-final double tabRadius = 10.0;
+const double tabRadius = 10.0;
+const Color selectedTabLabelColor = Colors.teal;
+final Color tabBgColor = Colors.teal[100]!;
+const Color unSelectedTabLabelColor = Colors.white;
+const Color tabBarBgColor = Colors.teal;
+
+///[Border Radius Constants]
+const BorderRadiusGeometry topRight =
+    BorderRadius.only(topRight: Radius.circular(tabRadius));
+
+const BorderRadiusGeometry topLeft =
+    BorderRadius.only(topLeft: Radius.circular(tabRadius));
+const BorderRadiusGeometry topLR = BorderRadius.only(
+    topLeft: Radius.circular(tabRadius), topRight: Radius.circular(tabRadius));
