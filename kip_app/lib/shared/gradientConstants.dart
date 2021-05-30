4e511714a30bbeb45f48436de7fa01e03040fb2e
repleeka <tungondugi@gmail.com>
2021-05-30 16:04:18 +0530

    defined gradient constants

--- kip_app/lib/shared/gradientConstants.dart
+++ kip_app/lib/shared/gradientConstants.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 ///[Color gradients]
 final Gradient verticalGradient = LinearGradient(
   colors: [
-    Colors.tealAccent[400]!,
-    Colors.cyan[600]!,
+    // Colors.tealAccent[400]!,
+    // Colors.cyan[600]!,
+    Color(0XFF00D2A1),
+    Color(0XFF00B9D6),
   ],
-  begin: FractionalOffset(0.0, 0.0),
-  end: FractionalOffset(0.0, 1.0),
-  stops: [-1.0, 1.0],
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  tileMode: TileMode.clamp,
+);
+final Gradient horizontalGradient = LinearGradient(
+  colors: [
+    Color(0XFF00D2A1),
+    Color(0XFF00B9D6),
+    // Color(0X00D2A0),
+  ],
+  begin: Alignment.bottomLeft,
+  end: Alignment.topRight,
   tileMode: TileMode.clamp,
 );
