dd1d0231a6ddfb3933a93c9f9db33e39f9ee5988
repleeka <tungondugi@gmail.com>
2021-06-11 04:54:45 +0530

    courseCards UI added to the homeCards widget

--- kip_app/lib/screens/cards/homeCard.dart
+++ kip_app/lib/screens/cards/homeCard.dart
@@ -11,34 +11,19 @@ class HomeCards extends StatefulWidget {
 class _HomeCardsState extends State<HomeCards> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: ScrollPhysics(),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Courses",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-            ),
-            SizedBox(
-              height: 100,
-              child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return Container();
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return ListView.builder(
+        padding: EdgeInsets.all(0),
+        shrinkWrap: true,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Course(
+            authorName: 'tungon dugi',
+            courseTitle:
+                '30 Days Flutter/Dart challenge for the beginners and the experts',
+            isBestseller: true,
+            imageLink: 'lib/assets/image/02.png',
+            rating: '5.0',
+          );
+        });
   }
 }
